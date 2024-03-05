1e2594e9d9fc2f1fcce96c2eb23d29792da21ef6
Kunwar Ujjwal Singh <kujjwal21378@gmail.com>
2024-03-05 19:59:49 +0530

    added some ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -45,8 +45,8 @@ function App() {
     <Todoprovider
       value={{ todos, addTodo, toggleComplete, updateTodo, deleteTodo }}
     >
-      <div className="bg-[#172842] min-h-screen py-9">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+      <div className="bg-[#172842] min-h-screen py-4">
+        <div className="w-full mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
